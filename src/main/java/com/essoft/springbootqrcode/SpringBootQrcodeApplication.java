fb6dc17e782db4307260d8d404d8a446296e26e4
ErtanSidar <essoft3@gmail.com>
2022-08-03 22:53:54 +0300

    project completed

---
+++ src/main/java/com/essoft/springbootqrcode/SpringBootQrcodeApplication.java
@@ -0,0 +1,13 @@
+package com.essoft.springbootqrcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQrcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQrcodeApplication.class, args);
+	}
+
+}
